144b07f9b1de5b968b872d71f71a3d3d28f3bc04
Yershek <homohojel3@gmail.com>
2026-01-17 08:18:14 +0600

    first 14

---
+++ src/main/java/kg/laponandsweezy/registrationlapon/config/LoggingAspect.java
@@ -0,0 +1,34 @@
+package kg.laponandsweezy.registrationlapon.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* kg.laponandsweezy.registrationlapon.service..*(..)) || execution(* kg.laponandsweezy.registrationlapon.controller..*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("Entering method: {} with arguments: {}", joinPoint.getSignature().toShortString(), Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(pointcut = "execution(* kg.laponandsweezy.registrationlapon.service..*(..)) || execution(* kg.laponandsweezy.registrationlapon.controller..*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("Exiting method: {} with result: {}", joinPoint.getSignature().toShortString(), result);
+    }
+
+    @AfterThrowing(pointcut = "execution(* kg.laponandsweezy.registrationlapon.service..*(..)) || execution(* kg.laponandsweezy.registrationlapon.controller..*(..))", throwing = "error")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
+        log.error("Exception in method: {} with cause: {}", joinPoint.getSignature().toShortString(), error.getMessage() != null ? error.getMessage() : "NULL");
+    }
+}
